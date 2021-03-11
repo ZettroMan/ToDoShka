de9f7482e39cc3587fcb8725c6384809192043f7
Roman Russkikh <r-ru@yandex.ru>
2021-03-11 23:25:08 +0300

    Сделали чекбоксы для поля "Статус" задач, добавили стиль отображения завершенных задач серым цветом и зачеркнутым; неприсвоенные значения (категория, приоритет, дата) также выводим серым цветом

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CategoriesComponent} from './views/categories/categories.component';
 import {TasksComponent} from './views/tasks/tasks.component';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
 
 @NgModule({
   declarations: [
@@ -14,6 +17,9 @@ import {TasksComponent} from './views/tasks/tasks.component';
   ],
   imports: [
     BrowserModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
     BrowserAnimationsModule
   ],
   providers: [],
